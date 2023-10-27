64da007dffd99538f50f93349f1e1a984b0cda9e
Steven Penny <spen+3052@protonmail.com>
2023-10-26 20:52:47 -0500

    +type Access_Token struct{}

--- blog/2023-10-26/play.go
+++ blog/2023-10-26/play.go
@@ -1,65 +1,68 @@
 package play
 
-import "154.pages.dev/protobuf"
+type Access_Token struct{}
 
-type Access_Token map[string]string
+type Acquire_Request struct{}
 
-func (Access_Token) Auth() string {
-   return ""
-}
-
-type Raw_Refresh_Token string
-
-func (*Raw_Refresh_Token) Post(oauth_token string) error {
+func (Acquire_Request) Do(doc string) error {
    return nil
 }
 
-func (Raw_Refresh_Token) Token() (Refresh_Token, error) {
+type Checkin struct{}
+
+type Delivery struct{}
+
+type Delivery_Request struct{}
+
+func (Delivery_Request) Do(doc string, vc uint64) (*Delivery, error) {
    return nil, nil
 }
 
-type Refresh_Token map[string]string
+type Details struct{}
 
-func (Refresh_Token) Post() (Access_Token, error) {
+type Details_Request struct{}
+
+func (Details_Request) Do(doc string) (*Details, error) {
    return nil, nil
-}
-
-func (Refresh_Token) Token() string {
-   return ""
 }
 
 type Device struct{}
 
-type Raw_Checkin []byte
-
-func (*Raw_Checkin) Post(Device) error {
-   return nil
-}
+type Raw_Checkin struct{}
 
 func (Raw_Checkin) Checkin() (*Checkin, error) {
    return nil, nil
 }
 
-type Checkin struct {
-   Message protobuf.Message
-}
-
-func (Checkin) Device_ID() (uint64, bool) {
-   return 0, false
-}
-
-// 1. to
-// 2. from
-// 3. field/method
-
-type Details struct {
-   Message protobuf.Message
-}
-
-func (*Details) Get(doc string) error {
+func (*Raw_Checkin) Do(Device) error {
    return nil
 }
 
-func (Details) Downloads() (uint64, bool) {
-   return 0, false
+type Raw_Refresh_Token struct{}
+
+func (*Raw_Refresh_Token) Do(oauth_token string) error {
+   return nil
+}
+
+func (Raw_Refresh_Token) Token() (*Refresh_Token, error) {
+   return nil, nil
+}
+
+type Refresh_Token struct{}
+
+func (Refresh_Token) Do() (*Access_Token, error) {
+   return nil, nil
+}
+
+type Sync_Request struct{}
+
+func (Sync_Request) Do(Device) error {
+   return nil
+}
+
+/////////////////////////
+
+type File struct {
+   Package_Name string
+   Version_Code uint64
 }
